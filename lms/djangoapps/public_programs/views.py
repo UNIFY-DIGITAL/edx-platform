970de4807f0d5429076ed7e9c5db6cd306e2aaa0
unknown <pokurumanojkumar58@gmail.com>
2025-11-26 14:54:22 +0530

    Program Code Changes

--- lms/djangoapps/public_programs/views.py
+++ lms/djangoapps/public_programs/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 def public_programs(request):
     api_url = "https://discovery.unify.university/api/v1/programs/"
-    programs = requests.get(api_url).json()
+    response = requests.get(api_url).json()
+
+    programs = response.get("results", [])
     return render(request, "public_programs/public_programs.html", {"programs": programs})
