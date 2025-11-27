b197e2d395e85386212ca8bf9d2f2e1e2e0d03cb
unknown <pokurumanojkumar58@gmail.com>
2025-11-27 16:15:16 +0530

    program code changes

--- lms/djangoapps/public_programs/views.py
+++ lms/djangoapps/public_programs/views.py
@@ -1,7 +1,20 @@
 import requests
 from django.shortcuts import render
 
-def public_programs(request):
+def public_programs(request, program_uuid=None):
+
+    
+    if program_uuid:
+        api_url = f"https://discovery.unify.university/api/v1/programs/{program_uuid}/"
+        response = requests.get(api_url)
+
+        if response.status_code != 200:
+            return render(request, "404.html", status=404)
+
+        program = response.json()
+        return render(request, "public_programs/program_detail.html", {"program": program})
+
+
     api_url = "https://discovery.unify.university/api/v1/programs/"
     response = requests.get(api_url).json()
 
