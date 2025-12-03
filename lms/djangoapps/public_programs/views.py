0521266b16020883e22ac9f8fa83568827cff775
unknown <pokurumanojkumar58@gmail.com>
2025-12-03 12:54:58 +0530

    Program UI design

--- lms/djangoapps/public_programs/views.py
+++ lms/djangoapps/public_programs/views.py
@@ -12,7 +12,7 @@ def public_programs(request, program_uuid=None):
             return render(request, "404.html", status=404)
 
         program = response.json()
-        return render(request, "public_programs/public_programs.html", {"program": program})
+        return render(request, "public_programs/public_programs.html", {"program": program, "programs": None})
 
 
     api_url = "https://discovery.unify.university/api/v1/programs/"
